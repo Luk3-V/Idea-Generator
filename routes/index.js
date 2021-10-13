bfcf564ba67e4e49eedc08d95071b2c3bfad572d
Luk3-V <38675245+Luk3-V@users.noreply.github.com>
2021-10-13 14:48:17 -0500

    Add get query for user settings

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const {generate} = require('../main/generator');
 
 router.get('/', function(req, res, next) {
 	console.log('index page loaded');
 	res.render('index');
+});
+
+router.get('/generate', function(req, res, next) {
+	console.log('generating...' + req.query);
+	let result = generate(req.query);
+	res.json(result);
 });
 
 module.exports = router;
